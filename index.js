0ca4fdccd19d95eb97f5fb758a2244a8a9404497
saniatalha <saniatalha0519@gmail.com>
2026-03-10 14:22:12 +0000

    Added all files

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const app = require('./app')
-const port = 3000
+require('dotenv').config()
+const port = process.env.PORT;
+const app = require("./app")
 
 app.listen(port, () => {
     console.log(`Fruit Api listening on port ${port}`);
-})
+});
